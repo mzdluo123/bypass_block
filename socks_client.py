3aeae9e4196e9e2287994b330ad4b7634c627c02
RainChan <mzdluo123@foxmail.com>
2021-10-07 16:54:29 +0800

    localdns

--- socks_client.py
+++ socks_client.py
@@ -2,6 +2,7 @@ import asyncio
 import ipaddress
 import random
 import struct
+import socket
 
 from config import SERVER_LIST, PROXY_PORT, BUFFER
 from print_bw import upload_queue, download_queue
@@ -10,7 +11,7 @@ from utils import copy
 
 async def handle_socks(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     # 获取握手包
-    socket = writer.transport.get_extra_info("peername")
+    connection = writer.transport.get_extra_info("peername")
     '''
     +----+----------+----------+
     |VER | NMETHODS | METHODS  |
@@ -50,14 +51,15 @@ async def handle_socks(reader: asyncio.StreamReader, writer: asyncio.StreamWrite
     if atype == 3:
         size = await reader.readexactly(1)
         address = await reader.readexactly(int.from_bytes(size, 'little'))
-        address = address.decode()
+        port = int.from_bytes(await reader.readexactly(2), 'big', signed=False)
+        address = await asyncio.get_running_loop().getaddrinfo(address.decode(), port,family=socket.AF_INET)
+        address = address[0][4][0]
     if atype == 1:
         address = ipaddress.IPv4Address(await reader.readexactly(4))
-
-    port = int.from_bytes(await reader.readexactly(2), 'big', signed=False)
+        port = int.from_bytes(await reader.readexactly(2), 'big', signed=False)
     proxy = random.choice(SERVER_LIST)
     pr, pw = await asyncio.open_connection(proxy, PROXY_PORT)
-    print(f"SOCKS {socket[0]}:{socket[1]} --{proxy}--> {address}:{port}")
+    print(f"SOCKS {connection[0]}:{connection[1]} --{proxy}--> {address}:{port}")
     pw.write(f'CONNECT {address}:{port} HTTP/1.1\r\n'.encode())
     pw.write(b"X-T5-Auth: ZjQxNDIh\r\n")
     pw.write(b'Proxy-Connection: Keep-Alive\r\n\r\n')
@@ -67,11 +69,11 @@ async def handle_socks(reader: asyncio.StreamReader, writer: asyncio.StreamWrite
 | 1  |  1  | X'00' |  1   | Variable |    2     |
 +----+-----+-------+------+----------+----------+
     '''
-    pack = struct.pack("!bbxb",5,0,atype)
+    pack = struct.pack("!bbxb", 5, 0, atype)
     writer.write(pack)
 
     if atype == 3:
-        writer.write(int.to_bytes(len(address),1,'little'))
+        writer.write(int.to_bytes(len(address), 1, 'little'))
         writer.write(address.encode())
     else:
         writer.write(int.to_bytes(int(address), 4, 'little'))
